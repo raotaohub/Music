2505aceedb7dfd5dfcc75cb24aecd70e041c228b
a3onny <a3onny@gmail.com>
2021-02-15 00:55:47 +0800

    chore(package): 添加i18n依赖

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'zh-cmn-Hans',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+};
