94d9ef0fec95918ff1d0654feec31deab965f4c8
a3onny <a3onny@gmail.com>
2021-02-16 22:41:05 +0800

    feat(config): 添加app配置文件

---
+++ src/config/app.ts
@@ -0,0 +1,13 @@
+const config: IappConfig = {
+  icon: '@/assets/logo.png',
+  repo: 'assone/music',
+  nav: [
+    { text: 'home', link: '/' },
+    { text: 'explore', link: '/explore' },
+    { text: 'library', link: '/library' },
+    { text: 'dynamic', link: '/dynamic' },
+  ],
+};
+
+export const { icon, repo, nav } = config;
+export default config;
