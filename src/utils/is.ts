dfdc60c78b53995bed1d4b3b3a011f1acf92127d
a3onny <a3onny@gmail.com>
2021-02-17 22:48:41 +0800

    feat(utils): is添加新的判断函数

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,2 +1,13 @@
+const { toString } = Object.prototype;
+
+export const isUndefined = (val: unknown) => typeof val === 'undefined';
+export const isString = (val: unknown) => typeof val === 'string';
+export const isNumber = (val: unknown) => typeof val === 'number';
+export const isObject = (val: unknown) => val !== null && typeof val === 'object';
+export const isArray = (val: unknown) => toString.call(val) === '[object Array]';
+
+export const isExternal = (val: string) => /^https?:\/\//i.test(val);
+
 export const isDev = process.env.NODE_ENV === 'development';
 export const isProd = process.env.NODE_ENV === 'production';
+export const isClient = !isUndefined(window);
